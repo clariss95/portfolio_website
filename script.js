b42479397ef5b31f938a5105c61c1931cfa57c51
Klára Šteflová <klara.steflova@gmail.com>
2022-07-03 17:40:07 -0230

    new design

---
+++ script.js
@@ -0,0 +1,33 @@
+const tl = gsap.timeline({ defaults: { duration: 2, ease: "power1.out" } })
+
+tl.fromTo('#Decoration', { opacity: 0 }, { opacity: 1, duration: 2 })
+tl.fromTo('#Coding', { scale: 0 }, { scale: 1, ease: "elastic.out(1.12)" })
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("projects");
+    let dots = document.getElementsByClassName("circle");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
