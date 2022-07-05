d9f165a519aab9cd23cdd35191bc016906fda007
Klára Šteflová <klara.steflova@gmail.com>
2022-07-05 21:11:57 -0230

    working links

--- script.js
+++ script.js
@@ -34,3 +34,69 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 }
+
+const nameInput = document.querySelector("#name");
+const email = document.querySelector("#email");
+const message = document.querySelector("#message");
+const success = document.querySelector("#success");
+const errorNodes = document.querySelectorAll(".error");
+
+
+function validateForm() {
+    clearMessages()
+    let errorFlag = false;
+    if (nameInput.value.length < 1) {
+        errorNodes[0].innerText = "Name cannot be blank.";
+        nameInput.classList.add("error-border")
+        errorFlag = true;
+    }
+    if (!emailIsValid(email.value)) {
+        errorNodes[1].innerText = "Invalid email address.";
+        email.classList.add("error-border")
+        errorFlag = true;
+    }
+
+    if (message.value.length < 1) {
+        errorNodes[2].innerText = "Please enter message.";
+        message.classList.add("error-border")
+        errorFlag = true;
+    }
+    if (!errorFlag) {
+        success.innerText = "Thank you!";
+    }
+}
+
+function clearMessages() {
+    for (let i = 0; i < errorNodes.length; i++) {
+        errorNodes[i].innerText = "";
+    }
+    success.innerText = "";
+    nameInput.classList.remove("error-border");
+    email.classList.remove("error-border");
+    message.classList.remove("error-border");
+}
+
+function emailIsValid(email) {
+    let pattern = /\S+@\S+\.\S+/;
+    return pattern.test(email);
+}
+
+function scrollWin() {
+
+    window.scrollBy({
+        top: 2800,
+        left: 0,
+        behavior: 'instant'
+    });
+
+}
+
+function scrollWin2() {
+
+    window.scrollBy({
+        top: 5000,
+        left: 0,
+        behavior: 'instant'
+    });
+
+}
